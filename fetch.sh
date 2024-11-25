c0311ceec0631b71b45b1bfaa86ec6ca4098449b
Jie Zhu <chore.08-protist@icloud.com>
2024-11-25 10:05:51 -0600

    update SVF path

--- fetch.sh
+++ fetch.sh
@@ -3,4 +3,4 @@ set -e
 
 # get SVF
 git clone --no-checkout https://github.com/SVF-tools/SVF.git "$FUZZER/repo/SVF"
-git -C "$FUZZER/SVF" checkout 3170e83b03eefc15e5a3707e5c52dc726ffcd60a
+git -C "$FUZZER/repo/SVF" checkout 3170e83b03eefc15e5a3707e5c52dc726ffcd60a
