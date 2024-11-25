521af05fa80e761570fcbc58f7ca1e4401a27a32
Jie Zhu <chore.08-protist@icloud.com>
2024-11-25 10:01:06 -0600

    add support for magma

---
+++ build.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+set -e
+set -x
+
+# Building SVF
+(
+    echo -e "## Build SVF"
+    pushd $FUZZER/SVF
+    sed -i 's/LLVMRELEASE=\/home\/ysui\/llvm-4.0.0\/llvm-4.0.0.obj/LLVMRELEASE=\/usr\/llvm/' build.sh
+    ./build.sh
+    popd
+)
+
+# Building precondInfer
+(
+    echo -e "## Build precondInfer"
+    pushd $FUZZER/precondInfer
+    mkdir -p build 
+    pushd build
+    cmake \
+        -DENABLE_KLEE_ASSERTS=ON \
+        -DCMAKE_BUILD_TYPE=Release \
+        -DLLVM_CONFIG_BINARY=/usr/bin/llvm-config \
+        -DSVF_ROOT_DIR=${FUZZER}/SVF \
+        -DSVF_LIB_DIR=${FUZZER}/SVF/Release-build/lib \
+        ..
+    make -j
+    mv bin/precondInfer $OUT/
+    popd
+    popd
+)
+
+# Building Ins
+(
+    echo -e "## Build Ins"
+    pushd $FUZZER/Ins
+    mkdir -p build 
+    pushd build
+    CXXFLAGS="-fno-rtti" cmake \
+        -DLLVM_DIR=/usr/lib/cmake/llvm/ \
+        -DCMAKE_BUILD_TYPE=Release \
+        ..
+    make -j
+    mv bin/Ins $OUT/
+    popd
+    popd
+)
--- fetch.sh
+++ fetch.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
 set -e
+
+# get SVF
+git clone --no-checkout https://github.com/SVF-tools/SVF.git "$FUZZER/repo/SVF"
+git -C "$FUZZER/SVF" checkout 3170e83b03eefc15e5a3707e5c52dc726ffcd60a
